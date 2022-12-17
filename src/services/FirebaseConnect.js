8b4aecd3012de4953a405ad714c63219fe1f6376
Ederson <edersonvidal137@gmail.com>
2022-12-17 19:08:38 -0300

    Ajustes nos arquivos de configuração

--- src/services/FirebaseConnect.js
+++ src/services/FirebaseConnect.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import * as firebaseConfig from "../config/firebaseConfig.json"
+import * as firebaseConfig from "../../config/firebaseConfig.json"
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
